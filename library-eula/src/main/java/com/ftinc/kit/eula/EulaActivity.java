ec29802ac7796088d84fe09553021d00b5074048
Drew Heavner <veedubusc@gmail.com>
2015-06-16 18:06:03 -0400

    Added new module 'Eula' for recreating a similar screen to the EULA screen in the new 2015 Google I/O app

---
+++ library-eula/src/main/java/com/ftinc/kit/eula/EulaActivity.java
@@ -0,0 +1,37 @@
+package com.ftinc.kit.eula;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Project: android-52Kit
+ * Package: com.ftinc.kit.eula
+ * Created by drew.heavner on 6/16/15.
+ */
+public class EulaActivity extends AppCompatActivity {
+
+    /***********************************************************************************************
+     *
+     * Constants
+     *
+     */
+
+
+    /***********************************************************************************************
+     *
+     * Variables
+     *
+     */
+
+    /***********************************************************************************************
+     *
+     * Lifecycle Methods
+     *
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eula);
+    }
+}
